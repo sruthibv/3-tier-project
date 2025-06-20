1cc8a97878e77257d58df7d7e26dba11a07ca65c
sruthibv <bsruthiuk@gmail.com>
2025-06-20 14:17:41 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://www.sruthilearns.online";
+const API_BASE_URL = "http://54.152.22.200:84";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
